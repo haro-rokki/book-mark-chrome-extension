80b08a5fb65ba45f6561aa19c2954e6b553ca27b
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2020-09-20 16:12:42 +0900

    リファクタリング

---
+++ src/components/article/paragraph.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css'
+
+type Props = {
+  p: string
+}
+
+export const Paragraph: React.FC<Props> = ({ p }) => {
+  return <div className={styles.paragraph}>{p}</div>
+}

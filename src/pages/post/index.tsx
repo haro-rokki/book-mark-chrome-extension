99f1b87f6c1654bba44b45fce79920cf2726af82
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2020-09-21 17:21:51 +0900

    投稿7

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,15 +1,20 @@
 import { NextPage } from 'next'
 import { useState, useCallback } from 'react'
+import { useRouter } from 'next/router'
 
 import { Editor } from '@/components/editor'
 import { SiteHeader, SiteHeaderItem } from '@/components/site-header'
 import { Button } from '@/components/button'
+import { usePostArticleMutation } from '@/generated/graphql'
 
 import styles from './index.module.css'
 
 const PostPage: NextPage = () => {
   const [subject, setSubject] = useState('')
   const [content, setContent] = useState('')
+  const [postArticle] = usePostArticleMutation()
+  const [postDisabled, setPostDisabled] = useState(false)
+  const router = useRouter()
 
   const handleChangeSubject = useCallback(
     (ev: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,12 +23,41 @@ const PostPage: NextPage = () => {
     [],
   )
 
+  const handlePost = useCallback(
+    async (ev: React.FormEvent<HTMLFormElement>) => {
+      ev.preventDefault()
+      if (!content || !subject || postDisabled) {
+        return
+      }
+
+      setPostDisabled(true)
+      const { data } = await postArticle({
+        variables: {
+          authorId: 'b351437f-a03b-4148-b581-22edc9151815',
+          content,
+          subject,
+          publishedAt: 'now()',
+        },
+      })
+      if (data && data.insert_articles_one) {
+        const articleId = data.insert_articles_one.id
+        router.push(`/hoge/${articleId}`)
+        setPostDisabled(false)
+      } else {
+        console.log('POST unknown state', data)
+      }
+    },
+    [content, subject, postDisabled, postArticle, router],
+  )
+
   const SiteHeaderRight = (
     <>
       <SiteHeaderItem>
-        <Button type="submit">
-          <span>投稿する</span>
-        </Button>
+        <form onSubmit={handlePost}>
+          <Button type="submit">
+            <span>投稿する</span>
+          </Button>
+        </form>
       </SiteHeaderItem>
       <SiteHeaderItem>
         <img className={styles.userIcon} src="/profile.jpg" />
@@ -49,9 +83,6 @@ const PostPage: NextPage = () => {
           onEdit={setContent}
         />
       </div>
-      <footer className={styles.footer}>
-        <Button className={styles.submitButton}>投稿する</Button>
-      </footer>
     </>
   )
 }

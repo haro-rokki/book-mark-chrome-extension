ff8eac9202898ea809a4c65baf18b507c1a9790c
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2020-09-21 11:36:45 +0900

    日付TDD-3

--- src/utils/date.test.ts
+++ src/utils/date.test.ts
@@ -4,7 +4,7 @@ describe('formatDate', () => {
   const now = new Date('2020-09-21 11:22:00')
   test('作成してすぐ', () => {
     expect(formatDate(new Date(now), now)).toEqual({
-      datetime: '2020/9/21 11:22:00',
+      datetime: '2020/09/21 11:22',
       isNew: true,
     })
   })
